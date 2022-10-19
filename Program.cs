bd7caacc41cb1d5f9b54a85701accbcd222987d6
saifmhr <saifmahar165@gmail.com>
2022-10-19 19:28:51 +0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Methods
+{
+ class Program
+    {
+        static void Main()
+        {
+            Program Names = new Program();
+            string N = Names.Names("saif");
+            Console.WriteLine(N);
+
+           Program.evenNumber(50);
+
+           Program sum=new Program();
+           int Sum =sum.Add(20, 10);
+            Console.WriteLine(Sum);
+
+        }
+
+
+        public int Add(int FN,int SN)
+        {
+            return FN + SN;
+        }
+        public static void evenNumber(int target)
+        {
+            int start = 0;
+
+            while (start <= target)
+            {
+                Console.WriteLine("Even numbers  "+ start);
+                start=start+2;
+            }
+        }
+
+        public string  Names(string name)
+        {
+
+            return(name);
+        }
+    }
+}
